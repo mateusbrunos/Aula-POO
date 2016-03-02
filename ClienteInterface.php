3686fae34a3ec89e6864465ea74f4a098e09dddc
mateusbrunos <mateusbruno_88@hotmail.com>
2016-03-02 16:20:18 -0300

    Added files via upload

---
+++ ClienteInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+interface ClienteInterface 
+{
+    public function getNome();
+    public function getCpf();
+    public function getEndereco();
+    public function getFone();            
+}
