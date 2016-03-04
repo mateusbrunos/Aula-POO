fbd4d2a670aa7260088e183e133e6700b7a5dd28
mateusbrunos <mateusbruno_88@hotmail.com>
2016-03-04 16:30:59 -0300

    Delete ClienteInterface.php

--- ClienteInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-interface ClienteInterface 
-{
-    public function getNome();
-    public function getCpf();
-    public function getEndereco();
-    public function getFone();            
-}
